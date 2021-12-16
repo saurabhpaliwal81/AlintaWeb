7d4f20c6ea2259ef135077414832d4bc0cc57f8b
Saurabh Paliwal <saurabh.paliwal@health.telstra.com>
2021-12-16 23:51:16 +1100

    removed commented code.

--- Application/DependencyInjection.cs
+++ Application/DependencyInjection.cs
@@ -9,14 +9,10 @@ namespace Application
     public static class DependencyInjection
     {
         public static IServiceCollection AddApplication(this IServiceCollection services)
-        {
-            //services.AddAutoMapper(Assembly.GetExecutingAssembly());
+        {           
             services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
             services.AddMediatR(Assembly.GetExecutingAssembly());
-            //services.AddTransient(typeof(IPipelineBehavior<,>), typeof(UnhandledExceptionBehaviour<,>));
-            //services.AddTransient(typeof(IPipelineBehavior<,>), typeof(AuthorizationBehaviour<,>));
             services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
-            //services.AddTransient(typeof(IPipelineBehavior<,>), typeof(PerformanceBehaviour<,>));
 
             return services;
         }
